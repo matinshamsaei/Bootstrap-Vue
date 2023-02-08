7b0de8367cf2282203108e20243aec7a54f8e1e8
ardalan shahandeh <ardalan.shahande@gmail.com>
2023-02-08 13:56:24 +0330

    faet: Convert Badge Component To TypeScript Lang

--- src/components/RBadge/index.vue
+++ src/components/RBadge/index.vue
@@ -1,68 +1,56 @@
-<script setup>
-import RLink from '../RLink/index.vue';
+<script setup lang="ts">
+import { useAttrs, reactive } from 'vue'
+import RLink from '@/components/RLink/index.vue'
 // import { defineAsyncComponent } from 'vue'
 // const RLink = defineAsyncComponent(() => import('../RLink/index.vue'))
 
-const props = defineProps({
-  variant: {
-    type: String,
-    default: 'success'
-  },
-  tag: {
-    type: String,
-    default: 'span',
-    validator(button) {
-      return ['span', 'a'].includes(button)
-    }
-  },
-  target: {
-    type: String,
-    default: '_self',
-    validator(value) {
-      return ['_self', '_blank', '_parent', '_top', 'framename'].includes(value)
-    }
-  },
-  pill: {
-    type: Boolean,
-    default: false
-  },
-  to: {
-    type: String,
-    default: null
-  },
-  href: {
-    type: String,
-    default: null
+type BadgeProps = {
+  variant?: string
+  tag?: 'span' | 'a'
+  target?: '_self' | '_blank' | '_parent' | '_top' | 'framename'
+  pill?: boolean
+  to?: string | null
+  href?: string | null
+}
+
+const badgeProps = withDefaults(defineProps<BadgeProps>(), {
+  variant: 'success',
+  tag: 'span',
+  target: '_self',
+  pill: false,
+  to: null,
+  href: null
+})
+
+const $badgeAttrs = useAttrs()
+
+const badgeAttrs = reactive({
+  ...$badgeAttrs
+})
+
+const badgeClass = [
+  'badge',
+  `text-bg-${badgeProps.variant}`,
+  {
+    'rounded-pill': badgeProps.pill
   }
-})
-
-const $attrs = useAttrs()
-
-const attrs = reactive({
-  ...$attrs,
-  class: [
-    'badge',
-    `text-bg-${props.variant}`,
-    {
-      'rounded-pill': props.pill
-    }
-  ]
-})
+]
 </script>
 
 <template>
-  <r-link
-    v-if="props.to || props.href"
-    v-bind="attrs"
-    :target="props.target"
-    :to="props.to"
-    :href="props.href"
+  <RLink
+    v-if="badgeProps.to || badgeProps.href"
+    v-bind="badgeAttrs"
+    :target="badgeProps.target"
+    :to="badgeProps.to"
+    :href="badgeProps.href"
+    :class="badgeClass"
     class="text-decoration-none"
   >
     <slot />
-  </r-link>
+  </RLink>
 
-  <component v-else :is="props.tag" v-bind="attrs" :target="props.target">
+  <component v-else :is="badgeProps.tag" v-bind="badgeAttrs" :target="badgeProps.target" :class="badgeClass">
     <slot />
   </component>
 </template>
