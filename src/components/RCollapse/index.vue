0c56cb5e77666d01e478b8fbf8a9929851a65ad0
ardalan shahandeh <ardalan.shahande@gmail.com>
2023-02-13 10:58:06 +0330

    feat: Convert Collapse Component To TypeScript Lang

--- src/components/RCollapse/index.vue
+++ src/components/RCollapse/index.vue
@@ -1,73 +1,128 @@
-<script setup>
-import { useAttrs, computed } from 'vue'
-import RButton from '../RButton/index.vue'
-// const RButton = defineAsyncComponent(() => import('@/components/RButton'))
+<script setup lang="ts">
+import type { ObjectHTMLAttributes } from 'vue'
+import { useAttrs, reactive, computed, onMounted, ref, watch } from 'vue'
 
-const props = defineProps({
-  id: {
-    type: String,
-    default: null
+type CollapseProps = {
+  modelValue?: boolean
+  id?: string | undefined
+  visible?: boolean
+  accordion?: boolean
+  tag?: string
+}
+
+const collapseProps = withDefaults(defineProps<CollapseProps>(), {
+  modelValue: false,
+  id: undefined,
+  visible: false,
+  accordion: false,
+  horizontal: false,
+  tag: 'div'
+})
+
+const transitioning = ref(false)
+
+function onEnter() {
+  transitioning.value = true
+}
+
+function onAfterEnter() {
+  transitioning.value = false
+  isActive.value = true
+}
+
+function onLeave() {
+  transitioning.value = true
+}
+
+function onAfterLeave() {
+  isVisible.value = false
+  transitioning.value = false
+}
+
+const $collapseAttrs = useAttrs()
+
+const collapseAttrs = reactive({
+  ...$collapseAttrs
+})
+
+interface collapseEmit {
+  (e: 'updated:modelValue', val: boolean): void
+}
+
+const isActive = ref<boolean>(false)
+
+const collapseEmit = defineEmits<collapseEmit>()
+
+const isVisible = computed({
+  get() {
+    return collapseProps.modelValue
   },
-  buttonText: {
-    type: String,
-    default: 'Click button'
-  },
-  visible: {
-    type: Boolean,
-    default: false
-  },
-  accordion: {
-    type: Boolean,
-    default: false
-  },
-  horizontal: {
-    type: Boolean,
-    default: false
-  },
-  variant: {
-    type: String,
-    default: 'secondary'
+  set(val) {
+    collapseEmit('updated:modelValue', val)
   }
 })
 
-const collapseId = computed(() => {
-  return `#${props.id}`
+const collapseClass = reactive([
+  'mb-1',
+  {
+    'collapse-horizontal': collapseProps.horizontal
+  }
+])
+
+const height = ref<string | null>('0px')
+const collapse = ref<Element | null>(null)
+const style: ObjectHTMLAttributes = reactive({})
+
+watch(transitioning, (val) => {
+  if (val) {
+    height.value = `${collapse.value && collapse.value.scrollHeight}px`
+    style.height = height.value
+  } else {
+    delete style.height
+  }
 })
-
-const $attrs = useAttrs()
-
-const attrs = {
-  ...$attrs,
-  class: [
-    'collapse',
-    `btn-${props.variant}`,
-    'mb-1',
-    {
-      'collapse-horizontal': props.horizontal,
-      show: props.visible
-    }
-  ]
-}
-
-const accordion = {
-  'd-block w-100': props.accordion
-}
 </script>
 
 <template>
-  <RButton
-    class="mb-1"
-    :class="accordion"
-    :variant="props.variant"
-    :href="collapseId"
-    :aria-expanded="props.visible"
-    :aria-controls="props.id"
-    data-bs-toggle="collapse"
+  <Transition
+    name="height"
+    mode="out-in"
+    @enter="onEnter"
+    @after-enter="onAfterEnter"
+    @leave="onLeave"
+    @after-leave="onAfterLeave"
   >
-    {{ props.buttonText }}
-  </RButton>
-
-  <div :id="props.id" v-bind="attrs">
-    <slot />
-  </div>
+    <component
+      ref="collapse"
+      v-show="isVisible"
+      :is="collapseProps.tag"
+      v-bind="collapseAttrs"
+      :class="[
+        collapseClass,
+        { show: isActive && !transitioning, collapse: !transitioning, collapsing: transitioning }
+      ]"
+      :style="style"
+    >
+      <slot />
+    </component>
+  </Transition>
 </template>
+
+<style scoped>
+.collapsing {
+  position: relative;
+  transition: height 0.35s ease;
+  height: 0;
+  overflow: hidden;
+}
+
+.height-enter-active,
+.height-leave-active {
+  transition: height 0.35s ease;
+}
+
+.height-enter-from,
+.height-leave-to {
+  height: 0;
+}
+</style>
