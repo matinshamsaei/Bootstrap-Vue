67608188961259367fe8112c1a92e2a310abe905
Amir Nasiri <amirnasiri9473@gmail.com>
2023-02-19 11:28:41 +0330

    fix: Remove Extra Code From App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,3 @@
-<script setup lang="ts">
-import { reactive } from 'vue'
-import RFormCheckbox from '@/components/RForm/RFormCheckbox/RFormCheckbox.vue'
-import RFormCheckboxGroup from '@/components/RForm/RFormCheckbox/RFormCheckboxGroup.vue'
+<script setup lang="ts"></script>
 
-const checkbox = reactive({
-  values: [],
-  options: [
-    { text: 'test1', value: 'test-1', disabled: true },
-    { text: 'test2', value: 'test-2' },
-    { text: 'test3', value: 'test-3' }
-  ]
-})
-</script>
-
-<template>
-  <div class="container mt-3">
-    <r-form-checkbox-group v-model="checkbox.values" :options="checkbox.options" label="test label" />
-    {{ checkbox.values }}
-  </div>
-</template>
+<template>Welcome To Routaa UI Kit</template>
