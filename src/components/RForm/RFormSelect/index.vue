2feb8c699404c6ff5849246b1470f5db94dc2fa4
Amir Nasiri <amirnasiri9473@gmail.com>
2023-02-25 11:06:35 +0330

    feat: Add Auto Focus In Select & Textarea And Improve Options Prop In Select

--- src/components/RForm/RFormSelect/index.vue
+++ src/components/RForm/RFormSelect/index.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 // import { defineAsyncComponent } from 'vue'
 // const RFormSelectOption = defineAsyncComponent(() => import('@/components/RForm/RFormSelect/RFormSelectOption.vue'))
-
-import { useAttrs } from 'vue'
+import { useAttrs, ref, onMounted } from 'vue'
 import RFormSelectOption from './RFormSelectOption.vue'
 
 export interface IObject {
@@ -11,22 +10,48 @@ export interface IObject {
 
 type Props = {
   modelValue: string | number | null
-  options?: IObject[]
+  options?: IObject[] | string[] | number[]
   size?: 'sm' | 'md' | 'lg'
   selectSize?: string | number
+  textField?: string
+  valueField?: string
+  autoFocus?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   size: 'md',
   textField: 'text',
-  valueField: 'value'
+  valueField: 'value',
+  autoFocus: false
 })
 
 const attrs = useAttrs()
+
+const select = ref<HTMLInputElement | null>(null)
+
+onMounted(() => {
+  if (props.autoFocus) select.value?.focus()
+})
+
+const value = (option: IObject | number | string) => {
+  if (typeof option === 'object') return option[props.valueField]
+  else if (typeof option === 'number' || 'string') return option
+}
+
+const text = (option: IObject | number | string) => {
+  if (typeof option === 'object') return option[props.textField]
+  else if (typeof option === 'number' || 'string') return option
+}
+
+const disabled = (option: IObject | number | string) => {
+  if (typeof option === 'object' && option.disabled) return true
+  else return false
+}
 </script>
 
 <template>
   <select
+    ref="select"
     v-model="props.modelValue"
     :size="props.selectSize"
     v-bind="attrs"
@@ -35,8 +60,8 @@ const attrs = useAttrs()
     @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
   >
     <template v-for="option in props.options">
-      <RFormSelectOption :value="option[props.valueField]" :disabled="option.disabled">
-        {{ option[props.textField] }}
+      <RFormSelectOption :value="value(option)" :disabled="disabled(option)">
+        {{ text(option) }}
       </RFormSelectOption>
     </template>
 
